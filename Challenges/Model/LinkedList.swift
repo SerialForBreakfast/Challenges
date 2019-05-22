e7050895d177e7843199523048a94d4799dab72f
Joe McCraw <joe@showblender.com>
2019-05-22 13:57:04 -0700

    started LinkedList need to look up other implementations

---
+++ Challenges/Model/LinkedList.swift
@@ -0,0 +1,18 @@
+//
+//  LinkedList.swift
+//  Challenges
+//
+//  Created by Joseph McCraw on 5/22/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import UIKit
+
+class LinkedList: NSObject {
+
+}
+
+
+class Node: NSObject {
+    
+}
