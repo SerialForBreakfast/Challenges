00cd16773a9c74d78568bf328504fcc7b41b98fd
Joe McCraw <joe@showblender.com>
2019-05-22 12:16:42 -0700

    Need to disambiguated requirements for AbsolutePermutations

---
+++ Challenges/Model/AbsolutePermutation.swift
@@ -0,0 +1,31 @@
+//
+//  AbsolutePermutation.swift
+//  Challenges
+//
+//  Created by Joseph McCraw on 5/22/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import UIKit
+
+class AbsolutePermutation: NSObject {
+//    var numbers: [Int]
+//    var string: String
+    
+//    init(numbers: [Int]) {
+//        self.numbers = numbers
+//    }
+//    init(string: String) {
+//        self.string = string
+//    }
+    //separate functions for numeric and alphabetic inputs
+    func numericPermutations(array: [Int]) -> [Int] {
+        return array
+    }
+    
+    func alphabeticPermutaions(string: String) -> String {
+        return string
+    }
+    
+    
+}
